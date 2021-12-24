f91903fe511a373adde4a7475aa846e29cecac83
hayashi <square.lead23@gmail.com>
2021-12-25 00:02:38 +0900

    画面レイアウトの作成

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Box, Flex } from "@chakra-ui/react";
 import Canvas from "./Components/Canvas";
 import sketch from "./sketch";
 
 function App() {
   return (
-    <div className="App">
-      <Canvas sketch={sketch} />
-    </div>
+    <Flex flexDirection="column" h="100vh">
+      <Box w="100%" h={64} bg="tomato"></Box>
+      <Flex w="100%" h="100%">
+        <Box w={480} h="100%" bg="purple"></Box>
+        <Box w="100%" h="100%" bg="teal" id="canvasArea">
+          <Canvas sketch={sketch} />
+        </Box>
+      </Flex>
+    </Flex>
   );
 }
 
