70f4a190ac7eaae243738ec3070d2cd59909cbdb
hayashi <square.lead23@gmail.com>
2021-12-25 19:38:48 +0900

    レイアウトをレスポンシブ表示に対応

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import { Box, Flex } from "@chakra-ui/react";
+import MediaQuery from "react-responsive";
+
 import Canvas from "./Components/Canvas";
 import sketch from "./sketch";
 
 function App() {
   return (
     <Flex flexDirection="column" h="100vh">
-      <Box w="100%" h={64} bg="tomato"></Box>
-      <Flex w="100%" h="100%">
-        <Box w={480} h="100%" bg="purple"></Box>
-        <Box w="100%" h="100%" bg="teal" id="canvasArea">
-          <Canvas sketch={sketch} />
-        </Box>
-      </Flex>
+      <Box w="100%" h={48} bg="tomato"></Box>
+      <MediaQuery query="(min-width: 820px)">
+        <Flex w="100%" h="100%" alignItems="stretch">
+          <Box flexBasis={320} flexGrow={0} bg="purple"></Box>
+          <Box flexGrow={1} bg="teal" id="canvasArea">
+            {/* <Canvas sketch={sketch} /> */}
+          </Box>
+        </Flex>
+      </MediaQuery>
+      <MediaQuery query="(max-width: 820px)">
+        <Flex w="100%" h="100%" flexDirection="column">
+          <Box flexBasis="300" flexGrow={0} bg="teal" id="canvasArea">
+            {/* <Canvas sketch={sketch} /> */}
+          </Box>
+          <Box flexGrow={1} bg="purple"></Box>
+        </Flex>
+      </MediaQuery>
     </Flex>
   );
 }
