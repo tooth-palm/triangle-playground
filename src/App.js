2f7fa527a25e18ca5d8dfbda4f3f469ce9e405eb
hayashi <square.lead23@gmail.com>
2021-12-26 07:54:59 +0900

    操作エリアを作成

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 import MediaQuery from "react-responsive";
 
 import Canvas from "./Components/Canvas";
+import Tools from "./Components/Tools";
 import sketch from "./sketch";
 
 function App() {
@@ -19,20 +20,24 @@ function App() {
           Generative Art Playground
         </Text>
       </Box>
-      <MediaQuery query="(min-width: 820px)">
+      <MediaQuery query="(min-width: 620px)">
         <Flex w="100%" h="100%" alignItems="stretch">
-          <Box flexBasis={320} flexGrow={0} bg="purple"></Box>
+          <Box flexBasis={320} flexGrow={0} bg="purple">
+            <Tools padding={5} />
+          </Box>
           <Box flexGrow={1} bg="teal" id="canvasArea">
             {/* <Canvas sketch={sketch} /> */}
           </Box>
         </Flex>
       </MediaQuery>
-      <MediaQuery query="(max-width: 820px)">
+      <MediaQuery query="(max-width: 620px)">
         <Flex w="100%" h="100%" flexDirection="column">
           <Box flexBasis="300" flexGrow={0} bg="teal" id="canvasArea">
             {/* <Canvas sketch={sketch} /> */}
           </Box>
-          <Box flexGrow={1} bg="purple"></Box>
+          <Box flexGrow={1} bg="purple">
+            <Tools padding={10} />
+          </Box>
         </Flex>
       </MediaQuery>
     </Flex>
