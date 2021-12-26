b73e8e92852ddc0621a2d5c5b1beed9b634a4a3b
hayashi <square.lead23@gmail.com>
2021-12-26 18:30:58 +0900

    CanvasAreaを作成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
 import MediaQuery from "react-responsive";
 
-import Canvas from "./Components/Canvas";
+import CanvasArea from "./Components/CanvasArea";
 import Tools from "./Components/Tools";
-import sketch from "./sketch";
 
 function App() {
   return (
@@ -26,14 +25,14 @@ function App() {
             <Tools padding={5} />
           </Box>
           <Box flexGrow={1} bg="teal" id="canvasArea">
-            {/* <Canvas sketch={sketch} /> */}
+            <CanvasArea />
           </Box>
         </Flex>
       </MediaQuery>
       <MediaQuery query="(max-width: 620px)">
         <Flex w="100%" h="100%" flexDirection="column">
           <Box flexBasis="300" flexGrow={0} bg="teal" id="canvasArea">
-            {/* <Canvas sketch={sketch} /> */}
+            <CanvasArea />
           </Box>
           <Box flexGrow={1} bg="purple">
             <Tools padding={10} />
